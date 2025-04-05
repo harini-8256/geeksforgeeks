cd00c1ae93c2ee066e07d70f72b0927a9a3f6fca
Harini <harinidharanikota156@gmail.com>
2025-04-05 18:15:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s;
+            s = sc.next();
+            
+            Solution ob = new Solution();
+            char res = ob.firstRep(s);
+            if (res == '#')
+                System.out.println(-1);
+            else
+                System.out.println(res);
+             
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    char firstRep(String S)
+    {
+        // your code here
+        char ch='#';
+        for(int i=0;i<S.length();i++){
+            for(int j=i+1;j<S.length();j++){
+                if(S.charAt(i)==S.charAt(j)){
+                    ch= S.charAt(i);
+                    return ch;
+                }
+            }
+        }
+        return ch;
+    }
+}
